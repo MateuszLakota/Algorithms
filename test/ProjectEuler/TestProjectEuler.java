4e7e847c21b69e5e58ce4aa540c175818975d69d
Mateusz <MateuszLakota@outlook.com>
2021-02-28 19:35:58 +0100

    Implement unit tests to solutions to problems 1-3.

--- test/ProjectEuler/TestProjectEuler.java
+++ test/ProjectEuler/TestProjectEuler.java
@@ -1,4 +1,21 @@
 package ProjectEuler;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class TestProjectEuler {
+    @Test
+    void testSolveProblem1() {
+        Assertions.assertEquals(233168, Problem1.solveProblem1());
+    }
+
+    @Test
+    void testSolveProblem2() {
+        Assertions.assertEquals(4613732, Problem2.solveProblem2());
+    }
+
+    @Test
+    void testSolveProblem3() {
+        Assertions.assertEquals(6857, Problem3.solveProblem3());
+    }
 }
