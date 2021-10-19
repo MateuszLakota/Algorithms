be249497478a54045e763952ec9cd67349ced84e
Mateusz Łakota <MateuszLakota@outlook.com>
2021-10-19 14:25:26 +0200

    Optimize computational complexity of solution to problem 7.

--- src/ProjectEuler/Problem7.java
+++ src/ProjectEuler/Problem7.java
@@ -14,8 +14,8 @@ class Problem7 {
         int soughtPrimeNumber = 0;
         outerLoop:
         for (int i = 17; numberOfPrimeNumbers < 10001; i += 2) {
-            if (!(i % 3 == 0 || i % 5 == 0 || i % 7 == 0 || i % 11 == 0 || i % 13 == 0)) {
-                for (int j = 17; j < i; j += 2) {
+            if (i % 3 != 0 && i % 5 != 0 && i % 7 != 0 && i % 11 != 0 && i % 13 != 0) {
+                for (int j = 17; j <= Math.sqrt(i); j += 2) {
                     if (i % j == 0) {
                         continue outerLoop;
                     }
